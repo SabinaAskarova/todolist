a6b275435f4f13973bbd18ed7a4a9fa582b96619
Sabina <80788531+SabinaAskarova@users.noreply.github.com>
2022-04-13 12:01:14 +0400

    Update main.js

--- layihe2/main.js
+++ layihe2/main.js
@@ -12,3 +12,15 @@ let btn = document.querySelector('.inpicon')
 btn.addEventListener('mouseon', (event) => {
     event.target.style.backgroundColor = 'red';
 })
+
+
+let list = document.querySelector('.list');
+
+let btn = document.querySelector('.inpicon')
+
+btn.addEventListener('click', (event) => {
+    let input1 = document.createElement('input');
+    input1.value = input.value
+    list.append(input1);
+    input.value = ''
+})
